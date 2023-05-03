159894ff3828e9c4c380b719382246f134259e6b
Azmal <azmalgazi8@gmail.com>
2023-05-03 14:44:38 +0600

    make to fake chef data and send

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const latesCategory = require('./data/latesarticles.json')
+const chefData = require('./data/chefs.json')
 
 app.use(cors());
 
@@ -15,9 +16,26 @@ app.get('/latesarticles',(req, res) =>{
     res.send(latesCategory)
 })
 
+app.get('/chefData', (req, res) =>{
+    res.send(chefData)
+})
+
+app.get('/chefData/:id', (req, res) =>{
+    const id = req.params.id
+    console.log(id);
+   if(id < 7) {
+    const chefDetails =chefData.find(d => d.id == id) 
+    res.send(chefDetails)
+   }
+   else{
+    res.send([])
+   }
+})
 
 app.listen(port, ()=>{
     console.log(`Chef-Recipe API is running on port:${port}`);
+
+   
 })
 
 
