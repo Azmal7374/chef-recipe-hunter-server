036e2ed7e1824a284d2e46d9a01acfce5c081183
Azmal <azmalgazi8@gmail.com>
2023-05-03 12:28:20 +0600

    Home page two more section completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const latesCategory = require('./data/latesarticles.json')
 
 app.use(cors());
 
@@ -11,7 +11,9 @@ app.get('/', (req, res) =>{
     res.send('Chef-Recipe-Hunter-Server');
 })
 
-
+app.get('/latesarticles',(req, res) =>{
+    res.send(latesCategory)
+})
 
 
 app.listen(port, ()=>{
